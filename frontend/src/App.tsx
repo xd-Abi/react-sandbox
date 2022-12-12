804564fcc7e9a5281a66addb9349872462b71fe8
abinayan_tie <abi@tie-international.com>
2022-12-12 11:30:17 +0100

    Redesigned sign up form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import {SignUp} from "./Components";
 
-import "primereact/resources/themes/lara-dark-blue/theme.css";
+import "primereact/resources/themes/lara-light-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
@@ -8,7 +8,11 @@ import "primeflex/primeflex.css";
 import "./Styles/App.css";
 
 const App = () => {
-  return <SignUp />;
+  return (
+    <div className="pt-8">
+      <SignUp />
+    </div>
+  );
 };
 
 export default App;
--- frontend/src/Components/SignUp/SignUp.tsx
+++ frontend/src/Components/SignUp/SignUp.tsx
@@ -20,119 +20,130 @@ const SignUp = () => {
 
   return (
     <Container>
-      <Card className="md:pr-5 md:pl-5 lg:pr-7 lg:pl-7">
-        <h1 className="mb-5">Sign Up</h1>
-        <form className="grid">
-          <div className="col-12 md:col-6">
-            <FormField
-              id="name"
-              label="Full Name"
-              required
-              control={control}
-              error={errors.name?.message}
-            />
-          </div>
-          <div className="col-12 md:col-6">
-            <FormField
-              id="username"
-              label="Username"
-              required
-              control={control}
-              error={errors.username?.message}
-            />
-          </div>
-          <div className="col-12 md:col-6">
-            <FormField
-              id="email"
-              label="Email"
-              required
-              control={control}
-              error={errors.email?.message}
-            />
-          </div>
-          <div className="col-12 md:col-6">
-            <FormCalendar
-              id="birthdate"
-              label="Date of Birth"
-              required
-              control={control}
-              error={errors.birthdate?.message}
-            />
-          </div>
-          <Divider align="center">
-            <span className="p-tag">Residence</span>
-          </Divider>
-          <div className="col-12 md:col-6">
-            <FormField
-              id="address"
-              label="Address"
-              required
-              control={control}
-              error={errors.address?.message}
-            />
-          </div>
-          <div className="col-12 md:col-6">
-            <FormField
-              id="city"
-              label="City"
-              required
-              control={control}
-              error={errors.city?.message}
-            />
-          </div>
-          <div className="col-12 md:col-6">
-            <FormField
-              id="postcode"
-              label="Postcode"
-              required
-              control={control}
-              error={errors.postcode?.message}
-            />
-          </div>
-          <div className="col-12 md:col-6">
-            <FormDropdown
-              id="country"
-              label="Country"
-              required
-              control={control}
-              options={[
-                {label: "Australia", value: "AU"},
-                {label: "Brazil", value: "BR"},
-                {label: "China", value: "CN"},
-                {label: "Egypt", value: "EG"},
-                {label: "France", value: "FR"},
-                {label: "Germany", value: "DE"},
-                {label: "India", value: "IN"},
-                {label: "Japan", value: "JP"},
-                {label: "Spain", value: "ES"},
-                {label: "United States", value: "US"},
-              ]}
-              error={errors.country?.message}
-            />
-          </div>
-          <Divider align="center">
-            <span className="p-tag">Password</span>
-          </Divider>
-          <div className="col-12 md:col-6">
-            <FormField
-              id="password"
-              label="Password"
-              required
-              control={control}
-              error={errors.password?.message}
-            />
-          </div>
-          <div className="col-12 md:col-6">
-            <FormField
-              id="repeatPassword"
-              label="Comfirm Password"
-              required
-              control={control}
-              error={errors.repeatPassword?.message}
-            />
-          </div>
-        </form>
-      </Card>
+      <div className="grid">
+        <div className="col-6">
+          <Card className="md:pr-5 md:pl-5 lg:pr-7 lg:pl-7">
+            <h1 className="mb-5">Sign Up</h1>
+            <form className="grid">
+              <div className="col-12 md:col-6">
+                <FormField
+                  id="name"
+                  label="Full Name"
+                  required
+                  control={control}
+                  error={errors.name?.message}
+                />
+              </div>
+              <div className="col-12 md:col-6">
+                <FormField
+                  id="username"
+                  label="Username"
+                  required
+                  control={control}
+                  error={errors.username?.message}
+                />
+              </div>
+              <div className="col-12 md:col-6">
+                <FormField
+                  id="email"
+                  label="Email"
+                  required
+                  control={control}
+                  error={errors.email?.message}
+                />
+              </div>
+              <div className="col-12 md:col-6">
+                <FormCalendar
+                  id="birthdate"
+                  label="Date of Birth"
+                  required
+                  control={control}
+                  error={errors.birthdate?.message}
+                />
+              </div>
+              <Divider align="center">
+                <span className="p-tag">Residence</span>
+              </Divider>
+              <div className="col-12 md:col-6">
+                <FormField
+                  id="address"
+                  label="Address"
+                  required
+                  control={control}
+                  error={errors.address?.message}
+                />
+              </div>
+              <div className="col-12 md:col-6">
+                <FormField
+                  id="city"
+                  label="City"
+                  required
+                  control={control}
+                  error={errors.city?.message}
+                />
+              </div>
+              <div className="col-12 md:col-6">
+                <FormField
+                  id="postcode"
+                  label="Postcode"
+                  required
+                  control={control}
+                  error={errors.postcode?.message}
+                />
+              </div>
+              <div className="col-12 md:col-6">
+                <FormDropdown
+                  id="country"
+                  label="Country"
+                  required
+                  control={control}
+                  options={[
+                    {label: "Australia", value: "AU"},
+                    {label: "Brazil", value: "BR"},
+                    {label: "China", value: "CN"},
+                    {label: "Egypt", value: "EG"},
+                    {label: "France", value: "FR"},
+                    {label: "Germany", value: "DE"},
+                    {label: "India", value: "IN"},
+                    {label: "Japan", value: "JP"},
+                    {label: "Spain", value: "ES"},
+                    {label: "United States", value: "US"},
+                  ]}
+                  error={errors.country?.message}
+                />
+              </div>
+              <Divider align="center">
+                <span className="p-tag">Password</span>
+              </Divider>
+              <div className="col-12 md:col-6">
+                <FormField
+                  id="password"
+                  label="Password"
+                  required
+                  control={control}
+                  error={errors.password?.message}
+                />
+              </div>
+              <div className="col-12 md:col-6">
+                <FormField
+                  id="repeatPassword"
+                  label="Comfirm Password"
+                  required
+                  control={control}
+                  error={errors.repeatPassword?.message}
+                />
+              </div>
+            </form>
+          </Card>
+        </div>
+        <div className="col-6 flex justify-content-center">
+          <img
+            src={process.env.PUBLIC_URL + "/images/sign_up_wallpaper.svg"}
+            alt="Sign Up"
+            width="80%"
+          />
+        </div>
+      </div>
     </Container>
   );
 };
