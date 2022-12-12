7e85b03174669340db6c7a8b0e884680da9abf35
abinayan_tie <abi@tie-international.com>
2022-12-12 08:57:53 +0100

    Added prime react

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,7 @@
+import "primereact/resources/themes/lara-dark-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+
 const App = () => {
   return <div></div>;
 };
