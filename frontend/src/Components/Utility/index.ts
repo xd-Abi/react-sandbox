e0366e3d0176a03a4c8df10406f0279a677fdfe0
abinayan_tie <abi@tie-international.com>
2022-12-12 15:41:47 +0100

    Removed unneeded components

--- frontend/src/Components/Utility/index.ts
+++ frontend/src/Components/Utility/index.ts
@@ -1,6 +1,3 @@
 import Container from "./Container";
-import FormCalendar from "./FormCalendar";
-import FormDropdown from "./FormDropdown";
-import FormField from "./FormField";
 
-export {Container, FormCalendar, FormDropdown, FormField};
+export {Container};
